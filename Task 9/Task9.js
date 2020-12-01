664bce27e741d25b7593f2a71a4ffb6976171f87
Pranavvks <71880387+Pranavvks@users.noreply.github.com>
2020-12-01 19:09:01 +0530

    Add files via upload

---
+++ Task 9/Task9.js
@@ -0,0 +1,14 @@
+var message ="Hi!";
+var time    = 10 ;
+var count   = 100 ;
+var timegap =  10 ;
+var  i      = 0 ;
+var  timer  = setInterval(function(){
+document.getElementsByClassName('composer_rich_textarea')[0].innerHTML=message;
+if(('.im_submit').trigger('mousedown'));
+i++ ;
+if(i==count)
+{
+clearInterval(timer);
+}
+}
